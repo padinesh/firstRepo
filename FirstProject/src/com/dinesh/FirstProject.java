38d093be213bb0dc3f9d1c400c8dbf158ed9cce6
Dinesh <padinesh.s@gmail.com>
2021-06-12 12:40:53 +0530

    Committing in Feature Branch

--- FirstProject/src/com/dinesh/FirstProject.java
+++ FirstProject/src/com/dinesh/FirstProject.java
@@ -3,6 +3,6 @@ package com.dinesh;
 public class FirstProject {
 	
 	public static void main(String[] args) {
-		System.out.println("Committing in feature branch");
+		System.out.println("Committing in Feature branch");
 	}
 }
